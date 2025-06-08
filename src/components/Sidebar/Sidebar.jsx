4b383c20e2009728c793898977f8aef943836d5e
Salsabila H <harlensalsabila@gmail.com>
2025-06-08 18:27:38 +0700

    update sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -26,7 +26,6 @@ const Sidebar = () => {
     { path: '/leaves-approval', icon: <IoPaperPlane />, textKey: 'sidebar.leavesApproval', permission: 'leaves-approval' },
     { path: '/leaves-history', icon: <IoPaperPlane />, textKey: 'sidebar.leavesHistory', permission: 'leaves-history' },
     { path: '/employees', icon: <FaUsers />, textKey: 'sidebar.employees', permission: 'employees' },
-    { path: '/department', icon: <FaBuilding />, textKey: 'sidebar.department', permission: 'department' },
     { path: '/departments', icon: <FaBuilding />, textKey: 'sidebar.department', permission: 'department' },
     { path: '/role', icon: <FaUserTie />, textKey: 'sidebar.role', permission: 'role' },
     { path: '/leave-setting', icon: <FaCog />, textKey: 'sidebar.leaveSetting', permission: 'leave-setting' },
