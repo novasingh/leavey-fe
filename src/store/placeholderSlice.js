e63b5275305ef8456a1d8ec3581bd4f4feffef79
Salsabila H <harlensalsabila@gmail.com>
2025-06-08 14:10:06 +0700

    fix: modal style

---
+++ src/store/placeholderSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const placeholderSlice = createSlice({
+    name: 'placeholder',
+    initialState: {},
+    reducers: {},
+});
+
+export default placeholderSlice.reducer;
