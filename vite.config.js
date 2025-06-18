ad7a24994ebd14da49dd385b832a182f77353522
novaingh <akashdeep@hiehq.com>
2025-06-19 00:22:34 +0530

    Add : url in vite

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // allow connections from any host
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
+      '/api': [
+        {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+        },
+        {
+          target: 'http://ec2-54-251-95-96.ap-southeast-1.compute.amazonaws.com',
+          changeOrigin: true,
+        },
+        {
+          target: 'https://ec2-54-251-95-96.ap-southeast-1.compute.amazonaws.com',
+          changeOrigin: true,
+        },
+      ],
     },
+    cors: true, // allow all origins
   },
 })
